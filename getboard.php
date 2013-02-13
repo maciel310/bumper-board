6906db623c96d45b4db0cc0a615054bd35c09a6f
maciel310 <maciel310@gmail.com>
2013-02-13 11:38:26 -0800

    Initial Commit

---
+++ getboard.php
@@ -0,0 +1,106 @@
+{
+	"title": "Gameshow",
+	"rows": 3,
+	"cols": 3,
+	"bumpers": [
+		{
+			"label": "Somebody That I Used To Know",
+			"src": "know.mp3",
+			"trackStart": 5,
+			"fadeIn": 1,
+			"background": true,
+			"loop": true,
+			"loopStart": 50,
+			"loopEnd": 240,
+			"volume": 1.0
+		},
+		{
+			"label": "Already Gone",
+			"src": "gone.mp3",
+			"trackStart": 11,
+			"fadeIn": 5,
+			"background": true,
+			"loop": true,
+			"loopStart": 50,
+			"loopEnd": 240,
+			"volume": 1
+		},
+		{
+			"label": "",
+			"src": "",
+			"trackStart": 0,
+			"fadeIn": 0,
+			"background": false,
+			"loop": false,
+			"loopStart": 0,
+			"loopEnd": 0,
+			"volume": 1.0
+		},
+		{
+			"label": "Right",
+			"src": "Right.mp3",
+			"trackStart": 0,
+			"fadeIn": 0,
+			"background": false,
+			"loop": false,
+			"loopStart": 0,
+			"loopEnd": 0,
+			"volume": 1.0
+		},
+		{
+			"label": "Right",
+			"src": "Right.mp3",
+			"trackStart": 0,
+			"fadeIn": 0,
+			"background": false,
+			"loop": false,
+			"loopStart": 0,
+			"loopEnd": 0,
+			"volume": 1.0
+		},
+		{
+			"label": "Right",
+			"src": "Right.mp3",
+			"trackStart": 0,
+			"fadeIn": 0,
+			"background": false,
+			"loop": false,
+			"loopStart": 0,
+			"loopEnd": 0,
+			"volume": 1.0
+		},
+		{
+			"label": "Wrong",
+			"src": "Wrong.mp3",
+			"trackStart": 0,
+			"fadeIn": 0,
+			"background": false,
+			"loop": false,
+			"loopStart": 0,
+			"loopEnd": 0,
+			"volume": 1.0
+		},
+		{
+			"label": "Wrong",
+			"src": "Wrong.mp3",
+			"trackStart": 0,
+			"fadeIn": 0,
+			"background": false,
+			"loop": false,
+			"loopStart": 0,
+			"loopEnd": 0,
+			"volume": 1.0
+		},
+		{
+			"label": "Wrong",
+			"src": "Wrong.mp3",
+			"trackStart": 0,
+			"fadeIn": 0,
+			"background": false,
+			"loop": false,
+			"loopStart": 0,
+			"loopEnd": 0,
+			"volume": 1.0
+		}
+	]
+}

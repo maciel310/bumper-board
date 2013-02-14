f2eeec6cf9ac492529258ae0e82af15af510421f
maciel310 <maciel310@gmail.com>
2013-02-13 16:45:20 -0800

    Fix tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -9,36 +9,21 @@ describe('my app', function() {
   });
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
+  it('should automatically redirect to /board when location hash/fragment is empty', function() {
+    expect(browser().location().url()).toBe("/board");
   });
 
 
-  describe('view1', function() {
+  describe('board', function() {
 
     beforeEach(function() {
-      browser().navigateTo('#/view1');
+      browser().navigateTo('#/board');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
+    it('should render the board when user navigates to /board', function() {
+      expect(element('[ng-view] #controls').text()).
+        toMatch(/Gameshow/);
     });
 
   });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,26 +2,11 @@
 
 /* jasmine specs for controllers go here */
 
-describe('MyCtrl1', function(){
-  var myCtrl1;
+describe('BoardCtrl', function(){
+  var boardCtrl;
 
   beforeEach(function(){
-    myCtrl1 = new MyCtrl1();
-  });
-
-
-  it('should ....', function() {
-    //spec body
-  });
-});
-
-
-describe('MyCtrl2', function(){
-  var myCtrl2;
-
-
-  beforeEach(function(){
-    myCtrl2 = new MyCtrl2();
+    boardCtrl = new BoardCtrl();
   });
 
 

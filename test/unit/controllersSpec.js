02dcd682e9bea56313ceb5ab526d5b2f2bdb1912
maciel310 <maciel310@gmail.com>
2013-02-13 16:55:20 -0800

    More test fixing

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,14 +3,52 @@
 /* jasmine specs for controllers go here */
 
 describe('BoardCtrl', function(){
-  var boardCtrl;
-
-  beforeEach(function(){
-    boardCtrl = new BoardCtrl();
-  });
-
-
-  it('should ....', function() {
-    //spec body
-  });
+	var boardCtrl;
+	
+	var scope, ctrl, $httpBackend;
+	
+	beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+		$httpBackend = _$httpBackend_;
+		$httpBackend.expectGET('getboard.json').
+		respond({
+			"title": "Gameshow",
+			"rows": 1,
+			"cols": 2,
+			"bumpers": [{
+				"label": "Somebody That I Used To Know",
+				"src": "/bumper-board/know.mp3",
+				"trackStart": 5,
+				"fadeIn": 1,
+				"background": true,
+				"loop": true,
+				"loopStart": 50,
+				"loopEnd": 240,
+				"volume": 1.0
+			},
+			{
+				"label": "Already Gone",
+				"src": "/bumper-board/gone.mp3",
+				"trackStart": 11,
+				"fadeIn": 5,
+				"background": true,
+				"loop": true,
+				"loopStart": 50,
+				"loopEnd": 240,
+				"volume": 1
+			}
+			]
+		});
+		
+		scope = $rootScope.$new();
+		ctrl = $controller(PhoneListCtrl, {$scope: scope});
+	}));
+	
+	beforeEach(function(){
+		boardCtrl = new BoardCtrl();
+	});
+	
+	
+	it('should ....', function() {
+		//spec body
+	});
 });

01a1fa653166888e0bf9f1cdf551a1bbfc4c2c03
maciel310 <maciel310@gmail.com>
2013-02-27 19:04:01 -0800

    Abstract decodeAudioData call to easier reuse or direct access from controller code

--- app/js/services.js
+++ app/js/services.js
@@ -13,29 +13,34 @@ var serviceModule = angular.module('bumperBoard.services', []);
 serviceModule.factory('audioDecoder', ['$http', '$q', function($http, $q) {
 	var audioContext = new webkitAudioContext();
 	
-	return {
+	var audioDecoder = {
 		loadFromURL: function(src) {
 			var def = $q.defer();
 			
 			$http.get(src, {responseType: 'arraybuffer'}).success(function(data) {
 				console.log("Request Complete");
 				
-				audioContext.decodeAudioData(data, function(buffer) {
-					console.log("Decoding Complete");
-					
-					def.resolve(buffer);
-					
-					angular.element('#board').scope().$apply();
-				}, function onError(e) {
-					def.reject("Error decoding audio stream");
-				});
+				audioDecoder.fromArrayBuffer(data, def);
 			}).error(function() {
 				def.reject("Error with HTTP Request");
 			});
 			
 			return def.promise;
+		},
+		fromArrayBuffer: function(data, def) {
+			audioContext.decodeAudioData(data, function(buffer) {
+				console.log("Decoding Complete");
+				
+				def.resolve(buffer);
+				
+				angular.element('#board').scope().$apply();
+			}, function onError(e) {
+				def.reject("Error decoding audio stream");
+			});
 		}
 	};
+	
+	return audioDecoder;
 }]);
 
 })();
